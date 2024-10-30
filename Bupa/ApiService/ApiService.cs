031165425456fae99240d26321e08950dc0fbb73
Samirashul <Elefant13!SamirSabbagh>
2024-10-30 10:52:42 +0000

    Add project files.

---
+++ Bupa/ApiService/ApiService.cs
@@ -0,0 +1,30 @@
+﻿using Bupa.Components;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+namespace Bupa.ApiService
+{
+    public class ApiService
+    {
+        private static readonly string _url = "https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration=XX10ABC";
+
+        static async Task<string> getMOT()
+        {
+            string result="";
+            using (var httpClient = new HttpClient())
+            {
+                // Set default request headers
+                httpClient.DefaultRequestHeaders.Add("x-api-key", "fZi8YcjrZN1cGkQeZP7Uaa4rTxua8HovaswPuIno");
+
+                // Make a request
+                HttpResponseMessage response = await httpClient.GetAsync(_url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    result = content;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Bupa/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace Bupa.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string PrimaryColour { get; set; }
+        public DateOnly motTestExpiryDate { get; set; }
+        public int LastMileage { get; set; }
+
+        public Car(string make, string model, string colour, DateOnly date, int lastMileage) 
+        {
+            this.Make = make;
+            this.Model = model;
+            this.PrimaryColour = colour;
+            this.motTestExpiryDate = date;
+            this.LastMileage = lastMileage;
+        }
+    }
+}
